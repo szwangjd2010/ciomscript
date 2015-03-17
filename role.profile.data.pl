8601b53ea70beedf675b6dd790c21471260f8080
lilongen <lilongen@163.com>
2015-03-17 10:23:27 +0800

    1

--- role.profile.data.pl
+++ role.profile.data.pl
@@ -6,60 +6,71 @@ use English;
 use Data::Dumper;
 use Data::UUID;
 use BashUtil;
+use String::Buffer;
 
 sub main();
+sub newLine($);
 
 my $bashUtil = new BashUtil(1);
-my $SqlTpl = "insert into core_position_position_map (pid, currentPosition, nextPosition) values (#pid#, '#currentPosition#', '#nextPosition#');";
 my $L1 = 1500;
 my $L2 = 10;
 my $L3 = 10;
 my $L4 = 10;
 
+my $buf;
+my $pid;
+my $hOut;
+my $ug;
+
+sub newLine($) {
+	$pid++;
+	my $current = shift;
+	my $next = $ug->create_str();
+	$buf->writeln("$pid,\"$current\",\"$next\"");
+
+	if ($pid % 100 == 0) {
+		print $hOut $buf->flush();
+	}
+
+	return $next;
+}
+
 sub main() {
-	my $ug = Data::UUID->new();
-	my $pid = 10;
-	my $sql = "";
+	$buf = String::Buffer->new();
+	$pid = 10;
+	if (!open($hOut, '>', "/tmp/out.sql")) {
+		output("Can not open /tmp/out.sql\n");
+		return 1;
+	}
+
+	$ug = Data::UUID->new();
+	my $head = "pid,currentPosition,nextPosition";
+	
 	my $uuid1 = "";
 	my $uuid2 = "";
 	my $uuid3 = "";
 	my $uuid4 = "";
+
+	$buf->writeln($head);
 	for (my $m1 = 0; $m1 < $L1; $m1++) {
 		$uuid1 = $ug->create_str();
 
 		for (my $m2 = 0; $m2 < $L2; $m2++) {
-			$pid++;
-			$uuid2 = $ug->create_str();
-			$sql = $SqlTpl;
-			$sql =~ s/#pid#/$pid/;
-			$sql =~ s/#currentPosition#/$uuid1/;
-			$sql =~ s/#nextPosition#/$uuid2/;
-			system("echo \"$sql\" >> /tmp/out.sql");
+			$uuid2 = newLine($uuid1);
 
 			for (my $m3 = 0; $m3 < $L3; $m3++) {
-				$pid++;
-				$uuid3 = $ug->create_str();
-
-				$sql = $SqlTpl;
-				$sql =~ s/#pid#/$pid/;
-				$sql =~ s/#currentPosition#/$uuid2/;
-				$sql =~ s/#nextPosition#/$uuid3/;
-				system("echo \"$sql\" >> /tmp/out.sql");
+				$uuid3 = newLine($uuid2);
 
 				for (my $m4 = 0; $m4 < $L4; $m4++) {
-					$pid++;
-					$uuid4 = $ug->create_str();
-
-					$sql = $SqlTpl;
-					$sql =~ s/#pid#/$pid/;
-					$sql =~ s/#currentPosition#/$uuid3/;
-					$sql =~ s/#nextPosition#/$uuid4/;
-					system("echo \"$sql\" >> /tmp/out.sql");				
+					newLine($uuid3);						
 				}								
 			}
 		}
 
 	}
+
+	print $hOut $buf->flush();
+	close($hOut);
 }
 
 main()
