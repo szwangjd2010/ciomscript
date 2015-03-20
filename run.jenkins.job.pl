bf8b02075f077a3bf142935f0152f16a172eb3fd
lilongen <lilongen@163.com>
2015-03-20 10:17:56 +0800

    1

--- run.jenkins.job.pl
+++ run.jenkins.job.pl
@@ -8,32 +8,13 @@ use CiomUtil;
 
 my $job = $ARGV[0];
 my $JobParameters = {
-	'v2.admin.to.aliyun' => {
-		'DeployToAliBack1' => 'YES',
-		'Passwordv2' => 'woyaobusuv2'
-	},
-	'v2.api.to.aliyun' => {
-		'DeployToAliApi1' => 'YES',
-		'DeployToAliApi2' => 'YES',
-		'Passwordv2' => 'woyaobusuv2'
-	},
-	'xx-api.to.production' => {
-		'DeployTo50000' => 'YES',
-		'DeployTo50005' => 'YES',
-		'DeployTo50012' => 'YES',
-		'Password' => 'woyaobusuxx'
-	},
-	'xx-back.to.production' => {
-		'DeployTo50000' => 'YES',
-		'DeployTo50005' => 'YES',
-		'DeployTo50012' => 'YES',
-		'Password' => 'woyaobusuxx'
-	},
-	'xx-api.to.test' 	=> {},
-	'xx-back.to.test' 	=> {},
-	'2.0-oaf' 			=> {},
-	'2.0-admin' 		=> {},
-	'2.0-api' 			=> {}
+	'__dev.env-yxt.web.deploy.to.host' => {
+		'ciomHost' => '172.17.128.243',
+		'ciomUserName' => 'ci',
+		'ciomUserPassword' => 'P@ss!23',
+		'ciomAppPackageFile' => 'C:\ciom.workspace\yxtweb.zip',
+		'ciomAppDeployedLocation' => 'C:\iis'
+	}
 };
 
 sub help() {
@@ -59,7 +40,7 @@ sub constructJenkinsParameters() {
 
 sub constructCmd() {
 	my $CmdPrefix = "java -jar /var/lib/jenkins/jenkins-cli.jar"
-		. " -s http://192.168.0.248:8080/"
+		. " -s http://172.17.128.240:8080/"
 		. " -i /var/lib/jenkins/.ssh/id_rsa"
 		. " build $job"
 		. " -s -v";
