f40effce862f4729e99967ab2630f6bdd1dcf078
lilongen <lilongen@163.com>
2015-04-01 16:29:30 +0800

    benchmark script is basic ok

--- benchmark/yxt.taurus.db.profile.data.pl
+++ benchmark/yxt.taurus.db.profile.data.pl
@@ -5,94 +5,94 @@ use strict;
 use English;
 use Data::Dumper;
 use Data::UUID;
-use CiomUtil;
 use String::Buffer;
 
 my $UPWD= '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92';
 my $Tabs= {
 	core_org => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_org.csv',
+		file=>'/tmp/ciom/core_org.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,code,orgName,siteName,domain'
 	},
 	core_orguser => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_orguser.csv',
+		file=>'/tmp/ciom/core_orguser.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,orgId,password,email,fullName,mobile'
 	},
 	core_user_role_map => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_user_role_map.csv',
+		file=>'/tmp/ciom/core_user_role_map.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,userId,roleId'#roleId: 100001-100005
 	},	
 	core_group => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_group.csv',
+		file=>'/tmp/ciom/core_group.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,orgId,name,description,type,status'#type: 1, status: 0-1
 	},
 	core_department => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_department.csv',
+		file=>'/tmp/ciom/core_department.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,orgId,parentId,departmentName'
 	},
 	core_knowledge => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_knowledge.csv',
+		file=>'/tmp/ciom/core_knowledge.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,orgId,title,kngType,fileType,fileId'#kngType: 1-6, fileType, 1-2
 	},
 	core_file_info => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_file_info.csv',
+		file=>'/tmp/ciom/core_file_info.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,name'
 	},
 	core_convert_item => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_convert_item.csv',
+		file=>'/tmp/ciom/core_convert_item.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,knowledgeId,fileId,format'
 	},
 	core_user_department_map => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_user_department_map.csv',
+		file=>'/tmp/ciom/core_user_department_map.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,orgId,userId,departmentId'
 	},
 	core_user_group_map => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_user_group_map.csv',
+		file=>'/tmp/ciom/core_user_group_map.csv',
 		buf => String::Buffer->new(),
 		cols => 'pid,groupId,userId,type'
 	},
 	core_user_knowledge => {
-		h => -1,
+		h => undef,
 		counter => 0,
-		file=>'/tmp/core_user_knowledge.csv',
+		file=>'/tmp/ciom/core_user_knowledge.csv',
 		buf => String::Buffer->new(),
-		cols => 'pid,orgId,userId,knowledgeId,kngType'
+		cols => 'pid,orgId,userId,knowledgeId'
 	}
 };
+my $ug = Data::UUID->new();	
 
 sub openFile() {
 	while ( my ($k, $v) = each(%{$Tabs}) ) {
         if (!open($v->{h}, '>', $v->{file})) {
-        	output("Can not open $v->{file} \n");
+        	print("Can not open $v->{file} \n");
         }
     }
 }
@@ -103,15 +103,27 @@ sub closeFile() {
     }	
 }
 
-my $ug = Data::UUID->new();	
+sub initTabHeader2Buf() {
+	while ( my ($k, $v) = each(%{$Tabs}) ) {
+        $v->{buf}->writeln("$k - ($v->{cols})");
+    }	
+}
+
+sub flushBuf2File() {
+	while ( my ($k, $v) = each(%{$Tabs}) ) {
+		my $h = $v->{h};
+        print $h $v->{buf}->flush();
+    }	
+}
 
 sub increaseCounterAndFlush($) {
 	my $tab = shift;
 
 	$tab->{counter}++;
 	if ($tab->{counter} % 100 == 0) {
-		print $tab->{h} $tab->{buf}->flush();
-	}	
+		my $h = $tab->{h};
+		print $h $tab->{buf}->flush();
+	}
 }
 
 sub core_org_line($) {
@@ -129,59 +141,57 @@ sub core_orguser_line($$) {
 	my $userId = shift;
 
 	my $tab = $Tabs->{core_orguser};
-	$tab->{buf}->writeln("$userId,$orgId,$UPWD,$userId@yxt.cn,fullName-$userId,mobile-$userId");
+	$tab->{buf}->writeln("$userId,$orgId,$UPWD,$userId\@yxt.cn,fullName-$userId,mobile-$userId");
 	increaseCounterAndFlush($tab);		
 }
 
-my $Role = [100001, 100002, 100003, 100004, 100005];
-sub core_user_role_map_line($$) {
+sub core_user_role_map_line($$$) {
 	#cols => 'pid,userId,roleId'
 	my $pid = shift;
 	my $userId = shift;
+	my $roleId = shift;
 
-	my $roleId = $Role->[($tab->{counter} % 5)]
 	my $tab = $Tabs->{core_user_role_map};
 	$tab->{buf}->writeln("$pid,$userId,$roleId");
 	increaseCounterAndFlush($tab);		
 }
 
-sub core_group_line($$) {
+sub core_group_line($$$) {
 	#cols => 'pid,orgId,name,description,type,status'#type: 1, status: 0-1
 	my $orgId = shift;
 	my $groupId = shift;
+	my $status = shift;
 
 	my $tab = $Tabs->{core_group};
-	my $type = $tab->{counter} % 2;
-	$tab->{buf}->writeln("$groupId,$orgId,groupName-$groupId,groupDescription-$groupId,$type");
+	$tab->{buf}->writeln("$groupId,$orgId,groupName-$groupId,groupDescription-$groupId,1,$status");
 	increaseCounterAndFlush($tab);		
 }
 
-sub core_department_line($$) {
+sub core_department_line($$$) {
 	#cols => 'pid,orgId,parentId,departmentName'
 	my $orgId = shift;
 	my $departmentId = shift;
-	my $parentId = shift || 'NULL';
+	my $parentId = shift;
 
 	my $tab = $Tabs->{core_department};
 	$tab->{buf}->writeln("$departmentId,$orgId,$parentId,departmentName-$departmentId");
 	increaseCounterAndFlush($tab);	
 }
 
-sub core_knowledge_line($$) {
+sub core_knowledge_line($$$$$) {
 	#cols => 'pid,orgId,title,kngType,fileType,fileId'#kngType: 1-6, fileType, 1-2
 	my $orgId = shift;
 	my $knowledgeId = shift;
 	my $fileId = shift;
+	my $kngType = shift;
+	my $fileType = shift;
 
 	my $tab = $Tabs->{core_knowledge};
-	my $kngType = $tab->{counter} % 7 || 1;
-	my $fileType = $tab->{counter} % 3 || 1;
-
 	$tab->{buf}->writeln("$knowledgeId,$orgId,title-$knowledgeId,$kngType,$fileType,$fileId");
 	increaseCounterAndFlush($tab);		
 }
 
-sub core_file_info_line($$) {
+sub core_file_info_line($) {
 	#cols => 'pid,name'
 	my $fileId = shift;
 
@@ -198,7 +208,7 @@ sub core_convert_item_line($$$$) {
 	my $format = shift;
 
 	my $tab = $Tabs->{core_convert_item};
-	$tab->{buf}->writeln("$itemId,knowledgeId,$fileId,$format");
+	$tab->{buf}->writeln("$itemId,$knowledgeId,$fileId,$format");
 	increaseCounterAndFlush($tab);		
 }
 
@@ -210,7 +220,7 @@ sub core_user_group_map_line($$$$) {
 	my $type = shift;
 
 	my $tab = $Tabs->{core_user_group_map};
-	$tab->{buf}->writeln("$pid,groupId,$userId,$type");
+	$tab->{buf}->writeln("$pid,$groupId,$userId,$type");
 	increaseCounterAndFlush($tab);		
 }
 sub generate_core_user_group_map($$) {
@@ -234,7 +244,39 @@ sub generate_core_user_group_map($$) {
 	}
 }
 
-sub core_user_department_map_line($$$$) {
+sub core_user_knowledge_line($$$$) {#($pid, $orgId, $userId, $knowledgeId)
+	#cols => 'pid,orgId,userId,knowledgeId'
+	my $pid = shift;
+	my $orgId = shift;
+	my $userId = shift;
+	my $knowledgeId = shift;
+
+	my $tab = $Tabs->{core_user_knowledge};
+	$tab->{buf}->writeln("$pid,$orgId,$userId,$knowledgeId");
+	increaseCounterAndFlush($tab);	
+}
+sub generate_core_user_knowledge($$$) {
+	#cols => 'pid,orgId,userId,knowledgeId'
+	my $orgId = shift;
+	my $Users = shift;
+	my $Knowledges = shift;
+
+	my $knowledgeCnt = $#{$Knowledges} + 1;
+	my $userCnt = $#{$Users} + 1;
+
+print "3333333333333333 $knowledgeCnt - $userCnt\n";
+	my $min = $knowledgeCnt > $userCnt ? $userCnt : $knowledgeCnt;
+
+	for (my $i = 0; $i < $min; $i++) {
+		my $pid = $ug->create_str();
+		my $userId = $Users->[$i];
+		my $knowledgeId = $Knowledges->[$i];
+print "3333333333333333 $knowledgeCnt - $userCnt\n";		
+		core_user_knowledge_line($pid, $orgId, $userId, $knowledgeId);
+	}
+}
+
+sub core_user_department_map_line($$$$$) {
 	#cols => 'pid,orgId,userId,userType,departmentId'
 	my $pid = shift;
 	my $orgId = shift;
@@ -243,7 +285,7 @@ sub core_user_department_map_line($$$$) {
 	my $departmentId = shift;
 
 	my $tab = $Tabs->{core_user_department_map};
-	$tab->{buf}->writeln("$pid,orgId,$userId,$userType,$departmentId");
+	$tab->{buf}->writeln("$pid,$orgId,$userId,$userType,$departmentId");
 	increaseCounterAndFlush($tab);		
 }
 sub generate_core_user_department_map($$$$$) {
@@ -295,9 +337,66 @@ sub generate_core_user_department_map($$$$$) {
 
 }
 
+sub SetCounts() {
+	#org count
+	our $core_org_count = 10000;
+
+	#100 group per org
+	our $core_group_count = 100;
+
+	#1000 user per org
+	our $core_orguser_count = 1000;
+
+	#10 1st-class department per org
+	#5  2nd-class department per 1st-class department
+	#5  3rd-class department per 2nd-class department
+	our $core_department_count_1st = 10;
+	our $core_department_count_2nd = 5;
+	our $core_department_count_3rd = 5;
+
+	#1000 knowledge per org
+	#5 differnt convert item file per file
+	our $core_knowledge_count = 300;
+	our $core_convert_item_count = 6;	
+}
+
+sub SetCounts_test() {
+	#org count
+	our $core_org_count = 1;
+
+	#100 group per org
+	our $core_group_count = 2;
+
+	#1000 user per org
+	our $core_orguser_count = 10;
+
+	#10 1st-class department per org
+	#5  2nd-class department per 1st-class department
+	#5  3rd-class department per 2nd-class department
+	our $core_department_count_1st = 2;
+	our $core_department_count_2nd = 1;
+	our $core_department_count_3rd = 1;
+
+	#1000 knowledge per org
+	#5 differnt convert item file per file
+	our $core_knowledge_count = 3;
+	our $core_convert_item_count = 2;	
+}
+
 my $ConvertedFormat = ['pdf', 'html4', 'html5', 'mp4', 'flv'];
-sub generateDataFile() {
-	my $core_org_count = 10000;
+my $Role = [100001, 100002, 100003, 100004, 100005];
+sub constructFile() {
+	openFile();
+
+	our $core_org_count;
+	our $core_group_count;
+	our $core_orguser_count;
+	our $core_department_count_1st;
+	our $core_department_count_2nd;
+	our $core_department_count_3rd;
+	our $core_knowledge_count;
+	our $core_convert_item_count;	
+
 	for (my $idx_core_org = 0; $idx_core_org < $core_org_count; $idx_core_org++) {
 		my $orgId = $ug->create_str();
 		core_org_line($orgId);
@@ -312,66 +411,57 @@ sub generateDataFile() {
 		my $Knowledges = [];
 		#End
 
-		#100 group per org
-		my $core_group_count = 100;
 		for (my $idx_core_group = 0; $idx_core_group < $core_group_count; $idx_core_group++) {
 			my $groupId = $ug->create_str();
-			core_group_line($orgId, $groupId);
+			my $status = $idx_core_group % 2;
+			core_group_line($orgId, $groupId, $status);
 
-			push @{$Groups} $groupId;
+			push(@{$Groups}, $groupId);
 		}
 
-		#1000 user per org
-		my $core_orguser_count = 1000;
 		for (my $idx_core_orguser = 0; $idx_core_orguser < $core_orguser_count; $idx_core_orguser++) {
 			my $userId = $ug->create_str();	
 			core_orguser_line($orgId, $userId);
 			
-			my $userRoleMapId = $ug->create_str();	
-			core_user_role_map_line($userRoleMapId, $userId);
+			my $pid = $ug->create_str();
+			my $roleId = $Role->[$idx_core_orguser % 5];
+			core_user_role_map_line($pid, $userId, $roleId);
 
-			push @{$Users} $userId;
+			push(@{$Users}, $userId);
 		}
 
 		
-		#10 1st-class department per org
-		#5  2nd-class department per 1st-class department
-		#5  3rd-class department per 2nd-class department
-		my $core_department_count_1st = 10;
-		my $core_department_count_2nd = 5;
-		my $core_department_count_3rd = 5;
 		for (my $idx_core_department_1st = 0; $idx_core_department_1st < $core_department_count_1st; $idx_core_department_1st++) {
 			my $departmentId_1st = $ug->create_str();
-			core_department($orgId, $departmentId_1st);
+			core_department_line($orgId, $departmentId_1st, 'NULL');
 
-			push @{$Departments_1st} $departmentId_1st;
+			push(@{$Departments_1st}, $departmentId_1st);
 
 			for (my $idx_core_department_2nd = 0; $idx_core_department_2nd < $core_department_count_2nd; $idx_core_department_2nd++) {
 				my $departmentId_2nd = $ug->create_str();
-				core_department($orgId, $departmentId_2nd ,$departmentId_1st);
+				core_department_line($orgId, $departmentId_2nd ,$departmentId_1st);
 
-				push @{$Departments_2nd} $departmentId_2nd;
+				push(@{$Departments_2nd}, $departmentId_2nd);
 
 				for (my $idx_core_department_3rd = 0; $idx_core_department_3rd < $core_department_count_3rd; $idx_core_department_3rd++) {
 					my $departmentId_3rd = $ug->create_str();
-					core_department($orgId, $departmentId_3rd ,$departmentId_2nd);
+					core_department_line($orgId, $departmentId_3rd ,$departmentId_2nd);
 
-					push @{$Departments_3rd} $departmentId_3rd;
+					push(@{$Departments_3rd}, $departmentId_3rd);
 				}
 			}
 		}
 
-		#1000 knowledge per org
-		#5 differnt convert item file per file
-		my $core_knowledge_count = 300;
-		my $core_convert_item_count = 6;
 		for (my $idx_core_knowledge = 0; $idx_core_knowledge < $core_knowledge_count; $idx_core_knowledge++) {
 			my $knowledgeId = $ug->create_str();
 			my $fileId = $ug->create_str();
-			core_knowledge_line($orgId, $knowledgeId, $fileId);
+			my $kngType = $idx_core_knowledge % 7 || 1;
+			my $fileType = $idx_core_knowledge % 3 || 1;
+
+			core_knowledge_line($orgId, $knowledgeId, $fileId, $kngType, $fileType);
 			core_file_info_line($fileId);
 
-			push @{$Knowledges} $knowledgeId;
+			push(@{$Knowledges}, $knowledgeId);
 
 			for (my $idx_core_convert_info = 0; $idx_core_convert_info < $core_convert_item_count; $idx_core_convert_info++) {
 				my $itemId = $ug->create_str();
@@ -380,21 +470,24 @@ sub generateDataFile() {
 			}
 		}
 
+		generate_core_user_group_map($Users, $Groups);
+		generate_core_user_department_map($orgId, $Departments_1st, $Departments_2nd, $Departments_3rd, $Users);
+		generate_core_user_knowledge($orgId, $Users, $Knowledges);
 	}
 
+	flushBuf2File();
+	closeFile();
 }
 
 sub importData2Db() {
-	system("perl -pE 's|#File#|$fileCsv|mg; s|#Table#|core_position_position_map|mg; s|#Column#|pid,currentPosition,nextPosition|mg;' mysql.load.data.from.file.tpl > _tmp_");
+	system("perl -pE 's|#File#|todo|mg; s|#Table#|core_position_position_map|mg; s|#Column#|pid,currentPosition,nextPosition|mg;' mysql.load.data.from.file.tpl > _tmp_");
 	system("mysql -h 172.17.128.231 -uroot -ppwdasdwx -e 'source ./_tmp_' yxt");
 }
 
 sub main() {
-	if ($#ARGV == -1) {
-		generateDataFile();
-	} else {
-		importData2Db();
-	}
+	SetCounts_test();
+	initTabHeader2Buf();
+	constructFile();
 }
 
 main();
--- test.pl
+++ test.pl
@@ -4,12 +4,15 @@ use English;
 use Data::Dumper;
 
 use CiomUtil;
+use Math;
 
 
 my $a = 6 % 7 || 1;
 
-
-print int(10/3);
+my $aa = {
+	a => 0
+};
+print Math.min(10, 9);
 
 
 #$util->remoteExec("192.168.0.125", "22", "echo aaa >> /tmp/_1; echo bbb >> /tmp/_1");
