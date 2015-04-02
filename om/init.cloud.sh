411b6df4efbdef66520da59cc3fa1a494e2aad70
lilongen <lilongen@163.com>
2015-04-02 12:26:21 +0800

    1

--- om/init.cloud.sh
+++ om/init.cloud.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
+#
 
-mount //10.10.71.137/data/ciomshare /data/ciomshare -o username=ciom,password=ciomYXT123
+CiomShare='/data/ciomshare'
 
-#change gateway to enable inertnet connection
-perl -i -pE 's|(GATEWAY=)(.*)|${1}10.10.73.151|mg' /etc/sysconfig/network-scripts/ifcfg-eth0
+mountCiomShare() {
+	mount //10.10.68.205/ciomshare /data/ciomshare -o username=ciom,password=ciomYXT123
+}
 
-#change max open file number permanently
-echo "* soft nofile 102400" >> /etc/security/limits.conf
-echo "* hard nofile 102400" >> /etc/security/limits.conf
+changeGateway() {
+	echo "change gateway..."
+	perl -i -pE 's|(GATEWAY=)(.*)|${1}10.10.73.151|mg' /etc/sysconfig/network-scripts/ifcfg-eth0
+}
 
+changeMaxOpenFile() {
+	echo "change max open file number permanently..."
+	echo '* soft nofile 102400' >> /etc/security/limits.conf
+	echo '* hard nofile 102400' >> /etc/security/limits.conf
+	echo 'session    required     pam_limits.so' >> /etc/pam.d/login 
+}
 
+installJDK() {
+	echo "install JDK..."
+	rpm -ivh $CiomShare/jdk-7u76-linux-x64.rpm
+}
+
+patchJDK() {
+	echo "patch jdk security..."
+	javaProfileShell="$CiomShare/java.sh"
+	local_policy="$CiomShare/local_policy.jar"
+	US_export_policy="$CiomShare/US_export_policy.jar"
+	jdkHome='/usr/java/jdk1.7.0_76'	
+	/bin/cp -rf $javaProfileShell /etc/profile.d/
+	/bin/cp -rf $local_policy $jdkHome/jre/lib/security/
+	/bin/cp -rf $US_export_policy $jdkHome/jre/lib/security/
+}
+
+main() {
+	#mountCiomShare
+	changeMaxOpenFile
+	changeGateway
+	installJDK
+	patchJDK
+}
+
+main
