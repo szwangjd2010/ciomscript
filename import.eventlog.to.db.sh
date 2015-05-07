58363240a0bdd23e4c661419c5269d9db7dfcd25
lilongen <lilongen@163.com>
2015-05-07 16:01:46 +0800

    1

--- import.eventlog.to.db.sh
+++ import.eventlog.to.db.sh
@@ -5,30 +5,29 @@ source ./ciom.mysql.util.sh
 
 
 logFileYMD=$(date -d '1 days ago' +%04Y%02m%02d)
-
-getEventLogFile() {
-	echo -n "/data/logs/yxt/event.$logFileYMD.log"
-}
+eventlogFile=""
 
 instanceSQLTpl() {
-	eventlogFile=$(getEventLogFile)
 	sed "s|#LogFile#|$eventlogFile|g" load.eventlog.to.db.tpl > load.eventlog.to.db.sql
 }
 
 logInfo() {
 	logFile='/data/logs/yxt/eventlog.csv2db.log'
-	eventlogFile=$(getEventLogFile)
-
 	records=$(wc -l $eventlogFile | gawk '{print $1}')
 	entry="$(getTimestamp) $eventlogFile $records imported"
+
+	cat load.eventlog.to.db.sql >> $logFile
+	echo >> $logFile
 	echo "$entry" >> $logFile
+	echo >> $logFile
 }
 
-
 main() {
-	instanceSQLTpl
-	execSQLFile 10.10.73.166 3306 yxt pwdasdwx load.eventlog.to.db.sql yxtlog
-	logInfo
+	for eventlogFile in $(find /opt/ws/tomcat[678]-[1-9]/logs -name event.$logFileYMD.log); do
+		instanceSQLTpl
+		execSQLFile 10.10.73.166 3306 yxt pwdasdwx load.eventlog.to.db.sql yxtlog
+		logInfo
+	done
 }
 
 main
