5806be9d0b2f0fde74981f9f6ef9a011dd19d008
lilongen <lilongen@163.com>
2015-07-09 16:20:51 +0800

    mnt slaves workspace scripts

---
+++ mount.slaves.workspac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mount $CIOM_SLAVE_WIN_WORKSPACE
+mount $CIOM_SLAVE_OSX_WORKSPACE
---
+++ unmount.slaves.workspac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+umount -l $CIOM_SLAVE_WIN_WORKSPACE
+umount -l $CIOM_SLAVE_OSX_WORKSPACE
