b0d0f74f3d3888c2af395c9c04219cb68f9d0ec7
lilongen <lilongen@163.com>
2015-08-17 15:31:15 +0800

    ping api sh

--- ping.api.sh
+++ ping.api.sh
@@ -2,13 +2,21 @@
 
 
 hosts="10.10.73.235 10.10.76.73 10.10.75.138"
+ports="8080 8081 8082 8083"
 
-for host in $hosts; do
-	for port in 8080 8081 8082 8083; do
-		echo $host:$port
-		time curl http://$host:$port/v1/serverstatus --connect-timeout 3
-		echo 
+pingapi() {
+	host=$1
+	port=$2
+
+	echo $host:$port
+	time curl http://$host:$port/v1/serverstatus --connect-timeout 3
+	echo 	
+}
+
+while [ True ]; do
+	for host in $hosts; do
+		for port in $ports; do
+			pingapi $host $port
+		done
 	done
 done
-
-
