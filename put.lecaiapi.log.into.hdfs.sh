20216363126f79c438feb6bb5c54a5394dc40c9b
lilongen <lilongen@163.com>
2015-11-05 09:52:06 +0800

    add daily log to full

--- put.lecaiapi.log.into.hdfs.sh
+++ put.lecaiapi.log.into.hdfs.sh
@@ -4,8 +4,11 @@
 dayAgo=${1:-1}
 logFileYMD=$(date -d "$dayAgo days ago" +%04Y%02m%02d)
 
-logFile="/usr/share/nginx/html/ciompub/lecai.api/$logFileYMD/time.$logFileYMD.all-instances.log"
+logRootLocation="/usr/share/nginx/html/ciompub/lecai.api"
+fullLogFile="time-full.log"
+logFile="$logRootLocation/$logFileYMD/time.$logFileYMD.all-instances.log"
 logFileHdfsLocation="hdfs://yxtdfs/log.api.lecai/"
 hdfsBin="/opt/hadoop-2.7.1/bin/hdfs"
 
-$hdfsBin dfs -put $logFile $logFileHdfsLocation
+$hdfsBin dfs -put "$logFile" "$logFileHdfsLocation"
+$hdfsBin dfs -appendToFile "$logFile" "$logFileHdfsLocation/$fullLogFile"
