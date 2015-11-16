0c7630bf0cf56482cd819bd376dc4bf19132fce5
lilongen <lilongen@163.com>
2015-11-16 16:47:28 +0800

    note - hadoop UnknownHostException

--- hadoop/test.java
+++ hadoop/test.java
@@ -7,10 +7,13 @@ class test
 	public static void main(String args[]) throws Exception { 
 		InetAddress Address = InetAddress.getLocalHost(); 
 		System.out.println(Address); 
+		
 		Address = InetAddress.getByName("starwave.com"); 
 		System.out.println(Address); 
+		
 		InetAddress SW[] = InetAddress.getAllByName("www.nba.com"); 
-		for (int i=0; i<SW.length; i++) 
-		System.out.println(SW[i]);
+		for (int i=0; i<SW.length; i++) {
+			System.out.println(SW[i]);
+		}
 	}
 }
