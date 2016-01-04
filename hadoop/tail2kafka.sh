11e96a2f7ac724a31666b11453cf03888aec85d8
lilongen <lilongen@163.com>
2016-01-04 17:55:12 +0800

    1

--- hadoop/tail2kafka.sh
+++ hadoop/tail2kafka.sh
@@ -8,8 +8,8 @@ products="qida"
 types="action"
 brokers="10.10.23.164"
 
-for i in $products; do
+for product in $products; do
 	for type in $types; do
-		find /data -name $i_$type.log -exec tail -F {} | kafkacat -b $brokers -t $i.$type & \;
+		find /data -name ${product}_${type}.log -exec tail -F {} \; | nohup kafkacat -b $brokers -t ${product}.${type} & 
 	done
 done
