f40c5f11d2c9a3e7928f95cda238dfe0ea7c5bba
lilongen <lilongen@163.com>
2016-01-06 14:49:27 +0800

    import reports csv to db

--- desc.report.tables.sh
+++ desc.report.tables.sh
@@ -2,5 +2,5 @@
 
 tables="rpt_all_data rpt_channel_daily rpt_channel_monthly rpt_channel_weekly rpt_daily rpt_monthly rpt_retention rpt_weekly"
 for table in $tables; do
-	echo -n "-- /* $table */ ";mysql -BN lecaireport -e "desc $table" | awk '{print $1}' | perl -pE 's|\n|,|g' | perl -pE 's|,$|\n|'
+	echo -n "--	/* $table */ ";mysql -BN lecaireport -e "desc $table" | awk '{print $1}' | perl -pE 's|\n|,|g' | perl -pE 's|,$|\n|'
 done
--- import.reports.to.db.sh
+++ import.reports.to.db.sh
@@ -3,23 +3,27 @@
 
 ymd=$(date -d '1 days ago' +%04Y%02m%02d)
 tplLocation="/data/ciomshare/ciom"
+logFile="ld.log"
 
 instanceLoadDataTpl() {
 	tableName=$1
-	tplInstance=load.data.$tableName
+	tplInstance=$2
 	/bin/cp -rf $tplLocation/load.data.tpl $tplInstance
 	perl -i -pE "s|^--(.*$tableName)|\1|g" $tplInstance
 	perl -i -pE "s|#ymd#|$ymd|g" $tplInstance
 }
 
 main() {
+	echo $ymd >> $logFile
 	re=".*/(rpt_all_data|rpt_channel_daily|rpt_channel_monthly|rpt_channel_weekly|rpt_daily|rpt_monthly|rpt_retention|rpt_weekly)\.$ymd\.log"
 	for tableName in $(find /data/ws-1/tomcat7-1/logs/yxt/ -regextype posix-extended -regex "$re" | grep -o -P 'rpt_\w+'); do
-		echo $tableName
-		instanceLoadDataTpl $tableName
-		cat load.data.$tableName
-		#mysql -h 10.10.66.88 -uyxt -ppwdasdwx -e "source load.data.$tableName" lecaireport 2>>_err
+		echo $tableName >> $logFile
+		tplInstance=load.data.$tableName
+		instanceLoadDataTpl $tableName $tplInstance
+		mysql -h 10.10.66.88 -uyxt -ppwdasdwx -e "source $tplInstance" lecaireport 2>>$logFile
+		#mysql -h 10.10.66.88 -uyxt -ppwdasdwx -e "show databases;" lecaireport 2>>$logFile
 	done
+	echo >> $logFile
 }
 
 main
