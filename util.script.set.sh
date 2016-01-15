ce8872b2d2615d555f503dfc81b4f47309041adc
lilongen <lilongen@163.com>
2016-01-15 10:00:56 +0800

    misc

--- util.script.set.sh
+++ util.script.set.sh
@@ -3,8 +3,6 @@ find /opt/tomcat7-* -name server.xml \
 -i.$(date +%04Y%02m%02d.%02k%02M%02S) \
 -p -e 's/acceptCount="\d+"/acceptCount="500"/g' {} \;
 
-rpm -ivh http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-8.noarch.rpm
-
 #disk write benchmark
 dd if=/dev/zero of=/tmp/1111 bs=4k count=16k conv=fdatasync
 
@@ -23,18 +21,17 @@ winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{HOST="172.17.128.
 openssl req -x509 -config cert.request.tpl -extensions 'lle exts' -nodes\
  -days 1000 -newkey rsa:1024 -keyout myserver.key -out myserver.crt
 
-
-perl -i.bak -p0E 
-    's|(<artifactId>api</artifactId>[^<]+<version>)(bbb)(</version>)|\1a\3|smg' pom.xml
+perl -i.bak -p0E 's|(<artifactId>api</artifactId>[^<]+<version>)(bbb)(</version>)|\1a\3|smg' pom.xml
 # named groups/backreferences
 (?<name>groupPattern)   \g{name} -> in pattern, $+{name} -> outside of pattern
 perl -p0E 's|(?<g1><stringProp name="Argument.name">ServerIP</stringProp>\s+<stringProp name="Argument.value">)[\w\.]+(?<g2></stringProp>)|$+{g1}1.1.1.1$+{g2}|smg' /tech/api.jmx | head -n 20
 
- A905295A5D17FB075828FD15BD36FD7D0B875F5B
-
-
-
 find /data/ -name catalina.sh -exec sed -i '/# OS specific support/i CATALINA_OPTS="-Djava.library.path=/usr/local/apr/lib"' {} \;
 find /data/ -name server.xml -exec sed -i 's/Http11NioProtocol/Http11AprProtocol/' {} \;
-
 find /data -name ${product}_${type}.log -exec sh -c "tail -F {} | kafkacat -b $brokers -t ${product}.${type} &" \;
+
+multibyte characters search
+perl -ne '/\x{7528}\x{6237}.*"502"/ && print' qida_action.20160113.all-instances.log
+python -c "print repr('用户登录'.decode('utf-8'))[2:-1]"
+python -c "import re;print re.sub('u(\w{4})', 'x{\g<1>}', repr(' 用户登录'.decode('utf-8'))[2:-1])"
+\x{7528}\x{6237}\x{767b}\x{5f55}
