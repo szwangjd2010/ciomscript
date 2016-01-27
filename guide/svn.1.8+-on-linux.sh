dcd7d9fb2570c142066faa1aa8ab7c81ca20b12e
lilongen <lilongen@163.com>
2016-01-27 15:14:19 +0800

    1

--- guide/svn.1.8+-on-linux.sh
+++ guide/svn.1.8+-on-linux.sh
@@ -1,6 +1,6 @@
 http://blog.csdn.net/clementad/article/details/46898091
 
-1. create wandisco-svn repo conf
+1. create wandisco-svn repo
 # vi /etc/yum.repos.d/wandisco-svn.repo
 [WandiscoSVN]
 name=Wandisco SVN Repo
@@ -8,10 +8,12 @@ baseurl=http://opensource.wandisco.com/centos/7/svn-1.9/RPMS/$basearch/
 enabled=1
 gpgcheck=0
 
+
+2. remove exists subversion packages, and clean yum cache
 # yum remove subversion*
 # yum clean all
 
-2. 开始安装
+3. install
 # yum install subversion
 if occur following error
 Error: Package: subversion-1.8.11-1.x86_64 (WandiscoSVN)
