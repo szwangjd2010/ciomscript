c7b82d358dd82c08794f441b9fdff6d1773599e0
lilongen <lilongen@163.com>
2016-03-16 17:50:28 +0800

    hue, kerberos hdp

--- test.pl
+++ test.pl
@@ -1,30 +1,8 @@
 #!/usr/bin/perl -W
 #
 
-use lib "$ENV{CIOM_SCRIPT_HOME}";
-use strict;
-use English;
-use Data::Dumper;
-use Data::UUID;
-use CiomUtil;
-my $ug = Data::UUID->new();	
+use MobileExecutors;
+$e=new MobileExecutors(1);
+print $e;
 
-my $api = "/v1/industries/0/positionfunctions/3e74bb02-4e9d-4b50-a696-856a59cd2b78/positions";
-my $orgId = "3e74bb02-4e9d-4b50-a696-856a59cd2b78";
-if ($api =~ m/(\Q$orgId\E)/) {
-	#print $1;
-}
-
-my $a = {
-	"a" => 1, 
-	"b" => 2
-};
-
-#%{$a} = ();
-#undef %{$a};
-
-print defined($a) . "\n";
-$a->{c} = 3;
-
-print "$a->{b}-release.apk";
 
