46dccd9420f920baabfc135a952f9b428ebd0131
lilongen <lilongen@163.com>
2016-05-11 17:46:35 +0800

    1

--- clean.data.sh
+++ clean.data.sh
@@ -1,84 +1,117 @@
 #!/bin/bash
 # 
 #
-env=${1:-Local}
-workspace=/sdc/ciompub/behavior/_clean
+product=${1:-qida}
+logRoot=/sdc/ciompub/behavior
+workspace=$logRoot/_clean
 
-getProdFile() {
-	echo -n "/data/ws-1/tomcat7-1/logs/yxt/qida_action.$1.log"
+getFileName() {
+	ymd=$1
+	echo -n "${product}_action.${ymd}.all-instances.log"
 }
 
-getLocalFile() {
-	echo -n "/sdc/ciompub/behavior/$1/qida_action.$1.all-instances.log"
+getFileOriginalFullPath() {
+	ymd=$1
+	echo -n $logRoot/$ymd/$(getFileName $ymd)
 }
 
-getFile() {
-	echo -n $(get${env}File $1)
+getFileOperatedFullPath() {
+	ymd=$1
+	echo -n $workspace/$ymd/$(getFileName $ymd)
 }
 
-showFieldsSeparatorInfo() {
+createYmdWorkspace() {
+	ymd=$1
+	fileOriginal=$2
+	fileOperated=$3
+	ymdLocation=$workspace/$ymd
+
+	mkdir -p $ymdLocation
+	/bin/cp -rf $fileOriginal $fileOperated
+}
+
+truncateLog4jPrefix() {
 	f=$1
-	awk -F','  '{printf "     ,: %02d", NF; exit}' $f
-	awk -F' '  '{printf "    \\s: %02d", NF; exit}' $f
-	awk -F'\t' '{printf "    \\t: %02d", NF; exit}' $f
-	echo
+	t0=$(date +%s)
+	perl -i.origin -pE 's/^.+ \[\w+\.java:\d+\] - //g' $f
+	t1=$(date +%s)
+	echo truncateLog4jPrefix: $(( $t1 - $t0 )) secs	
 }
-
-
 
 nullToEmpty() {
-	$f=$1
+	f=$1
 	t0=$(date +%s)
-	perl -pE 's/(?<=,)null(?=,)/""/g' $f > $f.nullToEmpty
+	perl -i.truncateLog4jPrefix -pE 's/(?<=,)null(?=,)/""/g' $f
 	t1=$(date +%s)
 	echo nullToEmpty: $(( $t1 - $t0 )) secs
 }
 
-tabToSpace() {
-	$f=$1
+filedValueTabToSpace() {
+	f=$1
 	t0=$(date +%s)
-	perl -pE 's/\t/ /g' $f
+	perl -i.nullToEmpty -pE 's/\t/ /g' $f
 	t1=$(date +%s)
-	echo tabToSpace: $(( $t1 - $t0 )) secs	
+	echo filedValueTabToSpace: $(( $t1 - $t0 )) secs	
 }
 
 FieldSeparator_CommaToTab() {
-	$f=$1
+	f=$1
 	t0=$(date +%s)
-	perl -pE 's/","/"\t"/g' $f
+	perl -i.filedValueTabToSpace -pE 's/","/"\t"/g' $f
 	t1=$(date +%s)
 	echo FieldSeparator_CommaToTab: $(( $t1 - $t0 )) secs	
 }
 
 removeFieldClosureSignDoubleQuotes() {
-	$f=$1
+	f=$1
 	t0=$(date +%s)
-	perl -pE 's/(^"|"$|(?<=\t)"|"(?=\t))//g' $f
+	perl -i.FieldSeparator_CommaToTab -pE 's/(^"|"$|(?<=\t)"|"(?=\t))//g' $f
 	t1=$(date +%s)
-	echo FieldSeparator_CommaToTab: $(( $t1 - $t0 )) secs	
+	echo removeFieldClosureSignDoubleQuotes: $(( $t1 - $t0 )) secs	
 }
 
-main () {
-	mkdir -p $workspace
-	for (( i=1; i<180; i++)); do
-		ymd=$(date -d "$i days ago" +%04Y%02m%02d)
-		mkdir -p $workspace/$ymd
+showFieldsSeparatorInfo() {
+	f=$1
+	echo -n "FS info - "
+	awk -F','  '{printf ",: %02d", NF; exit}' $f
+	awk -F' '  '{printf "  \\s: %02d", NF; exit}' $f
+	awk -F'\t' '{printf "  \\t: %02d", NF; exit}' $f
+	echo
+}
 
-		f=$(getFile $ymd)
-		if [ ! -e $f ]; then
+# first: 			FS->','		null value -> null 		field clouser sign -> '"'
+# after 20160226, 	FS->'\t'	null value -> ""		field clouser sign -> '"'
+# after 20160510, 	FS->'\t'	null value -> ""		field clouser sign -> no clouser sign
+main () {
+	begin="2015-12-30" # end date: 20160510
+	for (( i=0; i<=132; i++)); do
+		ymd=$(date -d "$begin +$i days" +%04Y%02m%02d)
+		fileOriginal=$(getFileOriginalFullPath $ymd)
+		if [ ! -e $fileOriginal ]; then
 			continue
 		fi
 
-		printf "%03d - %s - %s " $i $ymd $f
-
-
-		if (( $ymd > 2016026 )); then 
-			echo yes
-		else 
-			nullToEmpty $f
-
+		if [ "$ymd" = "20160226" ]; then
+			continue
 		fi
-		showFieldsSeparatorInfo $f
+
+		fileOperated=$(getFileOperatedFullPath $ymd)
+		createYmdWorkspace $ymd $fileOriginal $fileOperated
+
+		echo ------------------------------------------------------------
+		printf "%03d - %s - %s\n" $i $ymd $fileOperated
+
+		if (( $ymd < 20160226 )); then
+			truncateLog4jPrefix $fileOperated
+			nullToEmpty $fileOperated
+			filedValueTabToSpace $fileOperated
+			FieldSeparator_CommaToTab $fileOperated
+		fi
+		removeFieldClosureSignDoubleQuotes $fileOperated
+		
+		showFieldsSeparatorInfo $fileOperated
+
+		echo
 	done
 }
 
