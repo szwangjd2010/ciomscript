d5ce79b40abb1b4f34b7720abb5fa277a2e9f90c
lilongen <lilongen@163.com>
2016-05-12 17:15:49 +0800

    1

--- clean.log.wrapper.sh
+++ clean.log.wrapper.sh
@@ -3,11 +3,14 @@
 #
 
 Products="qida lecai mall wangxiao"
-#Types="action access"
-LogTypes="action"
+Types="action access"
 
 for product in $Products; do
 	for logType in $LogTypes; do
+		echo =====================================================================
+		echo "$product - $logType"
 		./clean.log.sh $product $logType
+		echo
+		echo
 	done
 done
