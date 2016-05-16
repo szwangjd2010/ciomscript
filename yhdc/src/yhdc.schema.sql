80d816786a28c095c7ba51ad32d1309c792c195c
lilongen <lilongen@163.com>
2016-05-16 12:29:45 +0800

    yhdc hive fmpp project

---
+++ yhdc/src/yhdc.schema.sql
@@ -0,0 +1,20 @@
+<#list Products as product>
+  <#list LogTypes as logType>
+  <#assign tableName = product + "_" + logType + "log">
+  <#assign filedsFile = "/include/" + logType + ".log.fields">
+-----------------------------------------------------------------------------
+DROP TABLE IF EXISTS ${tableName};
+CREATE TABLE ${tableName} (
+  <#include filedsFile>
+) 
+PARTITIONED BY (year int, month int) 
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+
+ALTER TABLE ${tableName} ADD PARTITION (year=2015, month=12);
+<#list 1..12 as month>
+ALTER TABLE ${tableName} ADD PARTITION (year=2016, month=${month});
+</#list>
+
+
+  </#list>
+</#list>
