f9541b8c3175c88e4a9bf846156e5ba784555995
lilongen <lilongen@163.com>
2016-05-20 16:18:58 +0800

    1

--- put.lecaiapi.log.into.hdfs.sh
+++ put.lecaiapi.log.into.hdfs.sh
@@ -5,14 +5,23 @@ dayAgo=${1:-1}
 logFileYMD=$(date -d "$dayAgo days ago" +%04Y%02m%02d)
 
 logRootLocation="/sdc/ciompub/lecai.api"
-logFileHdfsLocation="hdfs://hdc-54/raw/lecai/"
+logFileHdfsLocation="hdfs://hdc-54/raw/lecai"
 hdfsBin="/opt/hadoop-2.7.1/bin/hdfs"
 
 put() {
 	logType=$1
-	localLogFile="$logRootLocation/$logFileYMD/${logType}.$logFileYMD.all-instances.log"
-	$hdfsBin dfs -put $localLogFile $logFileHdfsLocation
-	$hdfsBin dfs -appendToFile $localLogFile $logFileHdfsLocation/${logType}-full.log
+	logFileName=${logType}.${logFileYMD}.all-instances.log
+	localLogFile="$logRootLocation/$logFileYMD/$logFileName"
+	hdfsFile=$logFileHdfsLocation/$logFileName
+	hdfsFullFile="$logFileHdfsLocation/${logType}-full.log"
+	$hdfsBin dfs -test -f $hdfsFile 
+	if [ $? -eq 0 ]; then
+		echo "already exists"
+		return
+	fi
+
+	$hdfsBin dfs -put $localLogFile $hdfsFile
+	$hdfsBin dfs -appendToFile $localLogFile $hdfsFullFile
 }
 
 main() {
