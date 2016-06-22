a16bfbca0fc1da12e166b918b873e0ea096bed3a
lilongen <lilongen@163.com>
2016-06-22 11:18:29 +0800

    pcp refactory

--- yhdc/log.common.sh
+++ yhdc/log.common.sh
@@ -3,9 +3,27 @@
 yesterday=$(date -d "1 days ago" +%04Y%02m%02d)
 begin=${1:-$yesterday}
 end=${2:-$yesterday}
-HostsLogPresentin=${3:-"10.10.125.17"}
-Products=${4:-"lecai wangxiao qida mall"}
-LogTypes=${5:-"action access"}
+logMetainfoFile=$CIOM_SCRIPT_HOME/yhdc/${3:-action.access.metainfo}
+
+getHostsLogPresentin() {
+	echo -n $(grep -o -P '^[\d\.]+' $logMetainfoFile | sort -u |  perl -pE 's/\n/ /g' | perl -pE 's/ $//')
+}
+
+getProducts() {
+	echo -n $(grep -o -P '(?<=^#)[\w]+' $logMetainfoFile | sort -u | perl -pE 's/\n/ /g' | perl -pE 's/ $//')
+}
+
+getLogTypes() {
+	echo -n $(grep -o -P '(?<=^logs: )[\w ]+' $logMetainfoFile)
+}
+
+HostsLogPresentin=$(getHostsLogPresentin)
+Products=$(getProducts)
+LogTypes=$(getLogTypes)
+
+echo $HostsLogPresentin
+echo $Products
+echo $LogTypes
 
 product=''
 logType=''
