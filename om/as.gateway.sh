327d0e9b5514f465e4b6890354283a3cfd054932
lilongen <lilongen@163.com>
2016-07-05 09:40:56 +0800

    1

---
+++ om/as.gateway.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -s 172.17.128.0/24 -d 10.10.0.0/16 -o tun0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o tun0 -j ACCEPT
