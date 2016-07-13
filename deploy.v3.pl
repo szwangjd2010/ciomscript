9fdbb24ebda867aaffb383a0992102acb0ba614a
lilongen <lilongen@163.com>
2016-07-13 15:21:02 +0800

    java deploy v3

---
+++ deploy.v3.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -W
+#
+
+use lib "$ENV{CIOM_SCRIPT_HOME}";
+use strict;
+use English;
+use Data::Dumper;
+use Cwd;
+use CiomUtil;
+
+my $version = $ARGV[0];
+my $cloudId = $ARGV[1];
+my $appName = $ARGV[2];
+my $asRoot = $ARGV[3] || 'NotAsRoot';
+
+my $ciomUtil = new CiomUtil(1);
+my $OldPwd = getcwd();
+
+my $AppCiomFile="$ENV{CIOM_VCA_HOME}/$version/pre/$cloudId/$appName.ciom";
+my $MyWorkspace="$ENV{WORKSPACE}/$appName/target";
+my $AppPackageFile="$appName.war";
+require $AppCiomFile;
+our $Cloud;
+
+sub enterWorkspace() {
+	chdir($MyWorkspace);
+}
+
+sub leaveWorkspace() {
+	chdir($OldPwd);
+}
+
+sub deploy() {
+	my $shDeploy2Host = "$ENV{CIOM_SCRIPT_HOME}/deploy.app.to.host.with.multi.tomcats.v3.sh";
+	
+	my $hosts = $Cloud->{hosts};
+	my $cnt = $#{$hosts} + 1;
+	for (my $i = 0; $i < $cnt; $i++) {
+		my $cmd = sprintf("%s %s %s %s %s \"%s\" \"%s\" %s %s",
+				$shDeploy2Host,
+				$hosts->[$i]->{ip},
+				$hosts->[$i]->{port} || $Cloud->{port},
+				$hosts->[$i]->{tomcatParent} || $Cloud->{tomcatParent},
+				$appName,
+				$Cloud->{appRepoURI},
+				$Cloud->{appRepoScpURI},
+				$Cloud->{remoteWorkspace},
+				$asRoot
+		);
+
+		$ciomUtil->exec($cmd);
+	}
+}
+
+sub main() {
+	enterWorkspace();
+	deploy();
+	leaveWorkspace();
+}
+
+main();
