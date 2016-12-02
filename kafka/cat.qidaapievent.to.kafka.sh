0b39bdc58adcd17a99f181034918003358e65138
lilongen <lilongen@gmail.com>
2016-12-02 16:10:40 +0800

    k cat

--- kafka/cat.qidaapievent.to.kafka.sh
+++ kafka/cat.qidaapievent.to.kafka.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #
 
+Broker=10.10.23.164
+Topic=yxt.qida.api.event
+
 one() {
-    tail -F /data/ws-[1-2]/tomcat7-1/logs/yxt/qidaapi_event.20161125.log \
+    tail -F /data/ws-[1-2]/tomcat7-1/logs/yxt/qidaapi_event.log \
         | grep -P '^\w{8}-1' \
-        | kafkacat -b 10.10.23.164 -t yxt.qida.api.event &
+        | kafkacat -b $Broker -t $Topic &
 }
 
 multi() {
     for i in 1 2; do
-        tail -F /data/ws-$i/tomcat7-1/logs/yxt/qidaapi_event.20161125.log \
-            | kafkacat -b 10.10.23.164 -t yxt.qida.api.event &
+        tail -F /data/ws-$i/tomcat7-1/logs/yxt/qidaapi_event.log \
+            | kafkacat -b $Broker -t $Topic &
     done
 }
 
