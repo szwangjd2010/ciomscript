1e6b71d246176eab3f32eb0e01a1f9e04efa687e
lilongen <lilongen@gmail.com>
2017-01-06 10:16:38 +0800

    1

---
+++ mongo/datav_master.meta.js
@@ -0,0 +1,14 @@
+/*
+ * useless data meta info
+ */
+
+var DatabaseURL = "localhost:27017/datav_master";
+
+var DefaultLifetime = 7; // days
+
+var CollectionsLifetime = { // days
+	"ip_info_count": 14,
+	"config": 30,
+	"time_curve": 30,
+	"time_percent": -1
+};
