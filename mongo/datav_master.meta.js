82f30cf1efa54a30eb0205f38bc973f1653e6d45
lilongen <lilongen@gmail.com>
2017-01-10 16:07:19 +0800

    mongo ops

--- mongo/datav_master.meta.js
+++ mongo/datav_master.meta.js
@@ -15,5 +15,10 @@ var CollectionsLifetime = { // days
 	"lecai_api_counter_info": -1,
 	"lecai_login_count_info": -1,
 	"qida_login_count": -1,
-	"wangxiao_login_count": -1
+	"wangxiao_login_count": -1,
+
+	"lecai_log_count": 1,
+	"lecai_log_window": 1,
+	"qida_log_count": 1,
+	"qida_log_window": 1
 };
