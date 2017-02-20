af1ae5a2dd10610a8c0eec113b5eefb0ca60d694
Administrator <admin@example.com>
2017-02-20 14:52:44 +0800

    init

---
+++ deploy.app.to.docker.host.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+source $CIOM_SCRIPT_HOME/ciom.util.sh
+if [ "$JENKINS_HOME" == "" ]; then
+	simulateJenkinsContainer
+fi
+
+host=$1
+port=$2
+webappsParent=$3
+appName=$4
+asRoot=${5:-NotAsRoot}
+
+AppPackageFile="$appName.war"
+MyWorkspace="$WORKSPACE/$appName/target"
+WebappsLocation="$webappsParent/webapps"
+
+appContextName=$appName
+
+
+enterWorkspace() {
+	execCmd "cd $MyWorkspace"
+	execRemoteCmd $host $port "mkdir -p $WebappsLocation"
+}
+
+leaveWorkspace() {
+	execCmd "cd $MyWorkspace"
+}
+
+uploadFiles() {
+	upload $AppPackageFile $host $port $webappsParent/
+}
+
+applyAppPackage() {
+	execRemoteCmd $host $port "unzip -o $webappsParent/$AppPackageFile -d $WebappsLocation/$appContextName"
+}
+
+backup() {
+	timestamp=$(date +%04Y%02m%02d.%02k%02M%02S)
+	execRemoteCmd $host $port "cd $WebappsLocation; tar -czvf $webappsParent/$AppPackageFile-$timestamp.tgz $appContextName"
+}
+
+clean() {
+	execRemoteCmd $host $port "cd $WebappsLocation; rm -rf *"	
+}
+
+setAppContextName() {
+	if [ $asRoot == "AsRoot" ]; then
+		appContextName='ROOT'
+	fi
+}
+
+preDeployApp() {
+	stopTomcats $host $port $tomcatParent
+	sleep 3
+}
+
+removeCatalinaLog() {
+	execRemoteCmd $host $port "rm -rf $tomcatParent/tomcat*/logs/catalina.out"
+}
+
+postDeployApp() {
+	removeCatalinaLog
+	startTomcats $host $port $tomcatParent
+}
+
+main() {
+	setAppContextName
+	enterWorkspace
+	backup
+	uploadFiles
+	clean
+	applyAppPackage
+	leaveWorkspace
+}
+
+main
+
+unsimulateJenkinsContainer
