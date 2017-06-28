c5e406f9e8221fa254cad03b47ab12f45c87c775
lilongen <lilongen@163.com>
2017-06-28 14:54:37 +0800

    update

--- update.vca.sh
+++ update.vca.sh
@@ -3,8 +3,14 @@
 
 version=$1
 cloudId=$2
-appName=$3
+appName=${3:-""}
+
+if [ "$appName" == "" ]; then
+	echo "[CIOM] SVN update for $version->$cloudId"
+else
+	echo "[CIOM] SVN update for $version->$cloudId->$appName"
+fi
 
 appVcaHome="$CIOM_VCA_HOME/$version/pre/$cloudId/$appName"
 cd $appVcaHome
-eval "svn --no-auth-cache $(cat $CIOM_SCRIPT_HOME/mobile.vca.repo.credential) update"
+eval "svn --no-auth-cache $(cat $CIOM_SCRIPT_HOME/svn/jenkins.credential) update"
